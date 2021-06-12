4f3bfef4d634fb9709c500a0674847ef1d4ab605
kyriri <git-kyriri@opayq.com>
2021-06-12 02:06:14 -0300

    Implement  creation of charge order through API

--- paynow/app/controllers/api/v1/charge_orders_controller.rb
+++ paynow/app/controllers/api/v1/charge_orders_controller.rb
@@ -1,7 +1,39 @@
 class Api::V1::ChargeOrdersController < ActionController::API
   
   def create
-    head 201
+    @seller_company = SellerCompany.find_by!(token: charge_params[:company_token])
+    if @seller_company then
+      @buyer = Buyer.find_by!(token: charge_params[:client_token])
+      @product = Product.find_by!(token: charge_params[:product_token])
+      @payment_route = PaymentRoute.find_by!(token: charge_params[:payment_method_token])
+
+      @new_charge_order = ChargeOrder
+        .create(due_date: charge_params[:due_date],
+                value_before_discount: @product.price,
+                payment_method_option_id: @payment_route.payment_method_option_id,
+                seller_company_id: @seller_company.id, 
+                buyer_id: @buyer.id, 
+                product_id: @product.id,
+                # buyer_email_address: charge_params[:buyer_email_address]
+               )
+      render json: { charge_order_token: @new_charge_order.token }, status: 201
+    else
+    end
+    
+    
   end
-  
+
+  private
+
+  def charge_params
+    params
+      .require(:charge_order)
+      .permit(:company_token, 
+              :client_token, 
+              :product_token,
+              :due_date,
+              :payment_method_token, 
+              :buyer_email_address,
+            )
+  end
 end
--- paynow/spec/requests/charges_api_spec.rb
+++ paynow/spec/requests/charges_api_spec.rb
@@ -3,10 +3,27 @@ require 'rails_helper'
 describe 'API receives request for' do
   context 'creating a new charge' do
     it 'and it\'s succesful' do
-      
-      post '/api/v1/charge_orders/'
+      buyer = Buyer.create!(name: 'Freddie Mercury', cpf: '428.091.154-19')
+      company = SellerCompany.create!(name: 'Gelato Lovers', formal_name: 'Frida Trevisi di Leonardo LTDA',
+                                      cnpj: '01.584.565/0001-26', billing_email: 'contabilidade@gelatolovers.com.br')
+      product = Product.create!(name: 'Pistacchio ice-cream', price: 15, seller_company_id: company.id)
+      payment_method = PaymentMethodOption.create!(category: 1, provider: 'Bank of Duckburg',
+                                                   fee_as_percentage: 5, max_fee_in_brl: 1000)
+      payment_route = PaymentRoute.create!(seller_company_id: company.id,
+                                            payment_method_option_id: payment_method.id)
 
+      post '/api/v1/charge_orders/', params: {
+        charge_order: {
+          company_token: company.token,
+          client_token: buyer.token, 
+          product_token: product.token,
+          payment_method_token: payment_route.token,
+          due_date: 3.days.from_now,
+          # buyer_email_address: 'f.mercury@coldmail.com',
+        }
+      }
       expect(response).to have_http_status :created # 201
+      expect(response.body).to include('order_token')
     end
   end
 end
